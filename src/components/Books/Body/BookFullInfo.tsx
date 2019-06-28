3b5689518d3745a783dce64256518ce021a6dcbd
Kamesas <semakaleksandr2014@gmail.com>
2019-06-28 18:58:28 +0300

    3d card done

--- src/components/Books/Body/BookFullInfo.tsx
+++ src/components/Books/Body/BookFullInfo.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import iBooks from "../../../interfaces/Book";
 import "./BookFullInfo.scss";
 
@@ -7,21 +7,46 @@ interface iBookInfo {
 }
 
 const BookFullInfo = (props: iBookInfo) => {
-  const moseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    console.log(e);
-  };
+  const divRef: any = React.createRef();
+  const headingRef: any = React.createRef();
+
+  useEffect(() => {
+    divRef.current.addEventListener("mousemove", function(e: any) {
+      const offsetY = e.offsetY;
+      const halfHigth = divRef.current.clientHeight / 2;
+      const offsetX = e.offsetX;
+      const halfWidth = divRef.current.clientWidth / 2;
+
+      divRef.current.style = `transform: rotateX(${(offsetY - halfHigth) /
+        5}deg) rotateY(${-(offsetX - halfWidth) / 5}deg)`;
+    });
+
+    // headingRef.current.addEventListener("mousemove", function(e: any) {
+    //   const offsetY = e.offsetY;
+    //   const halfHigth = divRef.current.clientHeight / 2;
+    //   const offsetX = e.offsetX;
+    //   const halfWidth = divRef.current.clientWidth / 2;
+
+    //   headingRef.current.style = `transform: rotateX(${(offsetY - halfHigth) /
+    //     5}deg) rotateY(${-(offsetX - halfWidth) / 5}deg)`;
+    // });
+  });
 
   return (
     <div className="BookFullInfo">
-      <div className="BookImg" onMouseMove={moseMove}>
+      <div className="BookImg">
+        <h1 className="BookHeading" ref={headingRef}>
+          Heading
+        </h1>
         <img
+          ref={divRef}
           className="BookImg-img"
           src="https://english-e-reader.net/covers/The_Judges_House-Bram_Stoker.jpg"
           alt="alt"
         />
       </div>
-      <h1>{props.bookInfo.bookName}</h1>
-      <h2>{props.bookInfo.bookAuthor}</h2>
+
+      {/* <h2>{props.bookInfo.bookAuthor}</h2> */}
     </div>
   );
 };
