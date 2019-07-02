90ab482b43e0c10c0a198aec54f5b60265a9e85a
Kamesas <semakaleksandr2014@gmail.com>
2019-07-02 17:32:14 +0300

    added function  rotatingDefault

--- src/components/Books/Body/BookFullInfo.tsx
+++ src/components/Books/Body/BookFullInfo.tsx
@@ -13,32 +13,42 @@ const BookFullInfo = (props: iBookInfo) => {
   const headingRef: any = React.createRef();
 
   useEffect(() => {
-    mainRef.current.addEventListener("mousemove", function(e: any) {
-      const offsetY = e.offsetY;
-      const offsetX = e.offsetX;
-
-      // image
-      const halfHigth = mainRef.current.clientHeight / 2;
-      const halfWidth = mainRef.current.clientWidth / 2;
-      const rotateX = offsetY - halfHigth;
-      const rotateY = offsetX - halfWidth;
-
-      divRef.current.style = `transform: rotateX(${rotateX /
-        20}deg) rotateY(${-rotateY / 20}deg)`;
-
-      // heading
-      headingRef.current.style = `transform: rotateX(${rotateX /
-        30}deg) rotateY(${-rotateY / 30}deg)`;
-    });
+    mainRef.current.addEventListener("mousemove", rotating);
+    mainRef.current.addEventListener("mouseleave", rotatingDefault);
   });
 
+  function rotating(e: any) {
+    const offsetY = e.offsetY;
+    const offsetX = e.offsetX;
+
+    const halfHigth = mainRef.current.clientHeight / 2;
+    const halfWidth = mainRef.current.clientWidth / 2;
+    const rotateX = offsetY - halfHigth;
+    const rotateY = offsetX - halfWidth;
+
+    // image
+    divRef.current.style = `transform: rotateX(${rotateX /
+      20}deg) rotateY(${-rotateY / 20}deg)`;
+
+    // heading
+    headingRef.current.style = `transform: rotateX(${rotateX /
+      27}deg) rotateY(${-rotateY / 27}deg)`;
+  }
+
+  function rotatingDefault() {
+    divRef.current.style = "transform: rotate(0deg)";
+    headingRef.current.style = "transform: rotate(0deg)";
+  }
+
   return (
-    <div className="BookFullInfo" ref={mainRef}>
-      <div className="BookHeading">
-        <h1 ref={headingRef}>Heading</h1>
-      </div>
-      <div className="BookImg">
-        <img ref={divRef} className="BookImg-img" src={car} alt="alt" />
+    <div className="RotationBlock">
+      <div className="BookFullInfo" ref={mainRef}>
+        <div className="BookHeading">
+          <h1 ref={headingRef}>Heading</h1>
+        </div>
+        <div className="BookImg">
+          <img ref={divRef} className="BookImg-img" src={car} alt="alt" />
+        </div>
       </div>
     </div>
   );
