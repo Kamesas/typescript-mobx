e409adc7fc52aa27269b79ebbdafe149ce17d1ba
Kamesas <semakaleksandr2014@gmail.com>
2019-07-04 18:23:59 +0300

    startt

---
+++ src/store/system/types.ts
@@ -0,0 +1,15 @@
+export interface SystemState {
+  loggedIn: boolean;
+  session: string;
+  userName: string;
+}
+
+// Describing the different ACTION NAMES available
+export const UPDATE_SESSION = "UPDATE_SESSION";
+
+interface UpdateSessionAction {
+  type: typeof UPDATE_SESSION;
+  payload: SystemState;
+}
+
+export type SystemActionTypes = UpdateSessionAction;
